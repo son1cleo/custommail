974335091a5013a2a79c922de5694f445d53d6de
son1cleo <ratibkhan907@gmail.com>
2024-07-15 13:51:48 +0600

    third commit

--- custommail/custommail/settings.py
+++ custommail/custommail/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(5@bbd8=hw!0t&rzl%t96q4=b&%ls@c+5gowu!fzra37*0j7uw'
+SECRET_KEY = 'django-insecure-8u8)(&0*c#cmtr=-m))i71!(#^*3-96#j8%ah#0btwzl7-x1^o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'useradmin',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- custommail/useradmin/views.py
+++ custommail/useradmin/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import auth,User
+from django.contrib import messages
+from .forms import UserRegisterForm,Loginform
+from django.contrib.auth import authenticate
 
 # Create your views here.
+def home (request):
+    return render(request,'useradmin/index.html')
+# register
+
+def signup(request):
+    form = UserRegisterForm()
+    if request.method == 'POST':
+
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Account created successfully')
+            return redirect('login')
+            #return redirect('')
+
+    context = {'form':form}
+
+    return render(request, 'useradmin/signup.html', context=context)
+
+# login
+def login(request):
+    form = Loginform()
+
+    if request.method == 'POST':
+        form = Loginform(request, data = request.POST)
+        if form.is_valid():
+            username = request.POST('username')
+            password = request.POST('password')
+            
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.info(request, 'Username or password is incorrect')
+
+    
+    context = {'form':form}
+    return render(request, 'useradmin/login.html', context=context)
+
+def createMail(request):
+    return render(request,'useradmin/createMail.html')
